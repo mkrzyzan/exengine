32bd06967290a5cf456ed5de5d83f2af6b7234d0
mkrzyzan <marcin.adam.k@gmail.com>
2019-04-27 13:40:32 +0800

    separate declaration from definition

--- beczka.cpp
+++ beczka.cpp
@@ -19,33 +19,44 @@ struct SingleProdSingleConsQueue {
   atomic<size_t> head, tail;
   T buffer[SIZE];
 
-  SingleProdSingleConsQueue () : head(0), tail(0) {
-    cout << boolalpha << "Lock free:" <<  this->isLockFree() << endl;
-  } 
+  SingleProdSingleConsQueue (); 
 
-  bool isLockFree() {
-    return head.is_lock_free() && tail.is_lock_free();
-  }
+  bool isLockFree();
 
-  bool push(T x) {
-    size_t current_head = head.load(memory_order_relaxed);
-    if (SIZE-1 == (current_head - tail.load(memory_order_acquire))) return false;
+  bool push(T x);
 
-    buffer[current_head % SIZE] = x;
-    head.store(current_head+1, memory_order_release);
-    return true;
-  }
-
-  bool pop(T& x) {
-    size_t current_tail = tail.load(memory_order_relaxed);
-    if (current_tail == head.load(memory_order_acquire)) return false;
-
-    x = buffer[current_tail % SIZE];
-    tail.store(current_tail+1, memory_order_release);
-    return true;
-  }
-
+  bool pop(T& x);
 };
+
+
+template <typename T, int SIZE>
+SingleProdSingleConsQueue<T,SIZE>::SingleProdSingleConsQueue () : head(0), tail(0) {} 
+
+template <typename T, int SIZE>
+bool SingleProdSingleConsQueue<T,SIZE>::isLockFree() {
+  return head.is_lock_free() && tail.is_lock_free();
+}
+
+template <typename T, int SIZE>
+bool SingleProdSingleConsQueue<T,SIZE>::push(T x) {
+  size_t current_head = head.load(memory_order_relaxed);
+  if (SIZE-1 == (current_head - tail.load(memory_order_acquire))) return false;
+
+  buffer[current_head % SIZE] = x;
+  head.store(current_head+1, memory_order_release);
+  return true;
+}
+
+template <typename T, int SIZE>
+bool SingleProdSingleConsQueue<T,SIZE>::pop(T& x) {
+  size_t current_tail = tail.load(memory_order_relaxed);
+  if (current_tail == head.load(memory_order_acquire)) return false;
+
+  x = buffer[current_tail % SIZE];
+  tail.store(current_tail+1, memory_order_release);
+  return true;
+}
+
 
 template <typename T>
 struct MultiProducerMultiConsumerQueue {
@@ -53,21 +64,30 @@ struct MultiProducerMultiConsumerQueue {
   condition_variable cv;
   queue<T> q;
 
-  void push(T x) {
-    unique_lock<mutex> lm(m);
-    q.push(x);
-    cv.notify_one();
-  }
+  void push(T x);
 
-  T pop() {
-    unique_lock<mutex> lm(m);
-    cv.wait(lm, [&](){ return false == q.empty(); });
-
-    T x = q.front();
-    q.pop();
-    return x;
-  }
+  T pop();
 };
+
+
+template <typename T>
+void MultiProducerMultiConsumerQueue<T>::push(T x) {
+  unique_lock<mutex> lm(m);
+  q.push(x);
+  cv.notify_one();
+}
+
+template <typename T>
+T MultiProducerMultiConsumerQueue<T>::pop() {
+  unique_lock<mutex> lm(m);
+  cv.wait(lm, [&](){ return false == q.empty(); });
+
+  T x = q.front();
+  q.pop();
+  return x;
+}
+
+
 
 
 //======================= MATCHING ENGINE ============================
@@ -99,7 +119,7 @@ struct Event {
 };
 
 struct Notifier {
-  Notifier() : isShutdown(false) {}
+  Notifier();
 
   SingleProdSingleConsQueue<Event, 512> events;
   unordered_map<uint16_t, SingleProdSingleConsQueue<Event, 512>*> clients;
@@ -107,35 +127,47 @@ struct Notifier {
 
   void registerClient(uint16_t id, SingleProdSingleConsQueue<Event, 512>* events);
 
-  void run() {
-    while (false == isShutdown) {
-      Event event;
-      if (true == events.pop(event))
-      {
-        switch(event.type)
-        {
-          case EventType::Exec:
-          case EventType::OrderPlaced:
-          {
-            clients[event.trader]->push(event);
-            break;
-          }
-          case EventType::Tick:
-          {
-            // publishing MarketData (TODO: needed?)
-            break;
-          }
-        }
+  void run();
+};
 
-        // logging events to file right here...
-      }
-      else
+
+
+Notifier::Notifier() : isShutdown(false) {}
+
+void Notifier::run() {
+  while (false == isShutdown) {
+    Event event;
+    if (true == events.pop(event))
+    {
+      switch(event.type)
       {
-        this_thread::yield(); //not needed if busy loop
+        case EventType::Exec:
+        case EventType::OrderPlaced:
+        {
+          clients[event.trader]->push(event);
+          break;
+        }
+        case EventType::Tick:
+        {
+          // publishing MarketData (TODO: needed?)
+          break;
+        }
       }
+
+      // logging events to file right here...
+    }
+    else
+    {
+      this_thread::yield(); //not needed if busy loop
     }
   }
-};
+}
+
+
+
+
+
+
 
 struct Book {
   Book() : actualSide(None) {}
@@ -144,67 +176,79 @@ struct Book {
 };
 
 struct Engine {
-  Engine(Notifier& notifier) : books(), notify(notifier), isShutdown(false) {
-    books['A'] = {};
-    books['B'] = {};
-    books['C'] = {};
-  }
+  Engine(Notifier& notifier);
 
   Notifier& notify;
   unordered_map<char, Book> books;
   MultiProducerMultiConsumerQueue<InputOrder> q;
   bool isShutdown;
 
-  void run() {
-    while (false == isShutdown)
-    {
-      // blocking call
-      InputOrder newOrder = q.pop(); 
-      placeOrder(newOrder.instrument, newOrder.side, newOrder.trader, newOrder.qty);
-    }
-  }
+  void run();
   
-  void placeOrder(char instrument, Side side, uint16_t trader, uint16_t qty) {
-    Book& book = books[instrument];
-    uint16_t remainQty = qty;
+  void placeOrder(char instrument, Side side, uint16_t trader, uint16_t qty);
+};
 
 
-    if (true == book.orders.empty() || side == book.actualSide) {
-      book.actualSide = side;
-      book.orders.push_back(Order{trader, qty, qty, side});
 
-      notify.events.push({OrderPlaced, instrument, trader, qty, side});
+Engine::Engine(Notifier& notifier) : books(), notify(notifier), isShutdown(false) {
+  books['A'] = {};
+  books['B'] = {};
+  books['C'] = {};
+}
+
+void Engine::run() {
+  while (false == isShutdown)
+  {
+    // blocking call
+    InputOrder newOrder = q.pop(); 
+    placeOrder(newOrder.instrument, newOrder.side, newOrder.trader, newOrder.qty);
+  }
+}
+
+void Engine::placeOrder(char instrument, Side side, uint16_t trader, uint16_t qty) {
+  Book& book = books[instrument];
+  uint16_t remainQty = qty;
+
+
+  if (true == book.orders.empty() || side == book.actualSide) {
+    book.actualSide = side;
+    book.orders.push_back(Order{trader, qty, qty, side});
+
+    notify.events.push({OrderPlaced, instrument, trader, qty, side});
+  }
+  else
+  {
+    while (false == book.orders.empty() && 0 != remainQty) {
+      Order& top = book.orders.front();
+      if (top.remainQty > remainQty)
+      {
+        top.remainQty -= remainQty;
+        remainQty = 0;
+      }
+      else {
+        remainQty -= top.remainQty;
+        book.orders.pop_front();
+
+        notify.events.push({Exec, instrument, top.trader, top.qty, top.side});
+      }
+    }
+
+    if (0 == remainQty)
+    {
+      notify.events.push({Exec, instrument, trader, qty, side});
     }
     else
     {
-      while (false == book.orders.empty() && 0 != remainQty) {
-        Order& top = book.orders.front();
-        if (top.remainQty > remainQty)
-        {
-          top.remainQty -= remainQty;
-          remainQty = 0;
-        }
-        else {
-          remainQty -= top.remainQty;
-          book.orders.pop_front();
-
-          notify.events.push({Exec, instrument, top.trader, top.qty, top.side});
-        }
-      }
-
-      if (0 == remainQty)
-      {
-        notify.events.push({Exec, instrument, trader, qty, side});
-      }
-      else
-      {
-        book.actualSide = side;
-        book.orders.push_back(Order{trader, qty, remainQty, side});
-        notify.events.push({OrderPlaced, instrument, trader, qty, side});
-      }
+      book.actualSide = side;
+      book.orders.push_back(Order{trader, qty, remainQty, side});
+      notify.events.push({OrderPlaced, instrument, trader, qty, side});
     }
   }
-};
+}
+
+
+
+
 
 struct TradingTool;
 struct Exchange {
@@ -217,45 +261,54 @@ struct Exchange {
 };
 
 struct TradingTool {
-  TradingTool(uint16_t identifier) : id(identifier), isShutdown(false) {}
+  TradingTool(uint16_t identifier);
 
   SingleProdSingleConsQueue<Event, 512> events;
   MultiProducerMultiConsumerQueue<InputOrder>* q;
   uint16_t id;
   bool isShutdown;
 
-  void connectTo(Exchange& ex) {
-    q = &ex.engine.q; 
-    ex.registerClient(id, this);
-  }
+  void connectTo(Exchange& ex);
 
-  void run() {
-    while (false == isShutdown)
+  void run(); 
+};
+
+
+
+TradingTool::TradingTool(uint16_t identifier) : id(identifier), isShutdown(false) {}
+
+void TradingTool::connectTo(Exchange& ex) {
+  q = &ex.engine.q; 
+  ex.registerClient(id, this);
+}
+
+void TradingTool::run() {
+  while (false == isShutdown)
+  {
+    Event event;
+    if (true == events.pop(event))
     {
-      Event event;
-      if (true == events.pop(event))
+      switch(event.type)
       {
-        switch(event.type)
+        case EventType::Exec:
         {
-          case EventType::Exec:
-          {
-            // handle exec
-            break;
-          }
-          case EventType::OrderPlaced:
-          {
-            // handle orderPlaced
-            break;
-          }
+          // handle exec
+          break;
+        }
+        case EventType::OrderPlaced:
+        {
+          // handle orderPlaced
+          break;
         }
       }
-      else
-      {
-        this_thread::yield(); // not needed if spin lock
-      }
+    }
+    else
+    {
+      this_thread::yield(); // not needed if spin lock
     }
   }
-};
+}
+
 
 void Exchange::registerClient(uint16_t id, TradingTool* client) {
   notif.registerClient(id, &client->events);
